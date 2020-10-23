a6e0b9e677269b69a5313d32b60b4c9cfccbe3aa
darshanmest47 <72611011+darshanmest47@users.noreply.github.com>
2020-10-23 20:17:36 +0530

    Update encap.py

--- oops/encap.py
+++ oops/encap.py
@@ -1,1 +1,19 @@
+# encapsulation is the process of bundling the data
+# and the methods in a single entity
 
+class A:
+  def __init__(self):
+    self.__x = 20
+
+  def get(self):
+    print(f'Getting value....')
+    print(f'Got value {self.__x}')
+
+  def set(self,val):
+    self.__x = val
+    print(f'set value to {val}')
+
+a = A()
+a.get()
+a.set(3000)
+a.get()
